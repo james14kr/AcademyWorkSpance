ac1134ad8ca82e81cdff3e22df4cd4bf5a8ea3d0
james14kr <hwangms14kr@naver.com>
2026-01-15 17:24:27 +0900

    docs: React 폼(Form) 학습 프로젝트 초기 생성

---
+++ react/form/src/Input2.jsx
@@ -0,0 +1,22 @@
+import React, { use, useState } from 'react'
+
+const Input2 = () => {
+
+  const [data1, setData1] = useState('');
+  const [data2, setData2] = useState('');
+
+  console.log(data1, data2);
+
+  return (
+    <>
+    
+      <input type="text" value={data1} onChange={ e => setData1(e.target.value)} />
+
+      <input type="text" value={data2} onChange={ e => setData2(e.target.value)} />
+
+
+    </>
+  )
+}
+
+export default Input2
