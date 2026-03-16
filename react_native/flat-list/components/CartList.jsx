43ecd53c6b6d989fd678ee8566f7008628eaa213
james14kr <hwangms14kr@naver.com>
2026-03-16 17:38:31 +0900

    react-native 학습 내용 업로드

--- react_native/flat-list/components/CartList.jsx
+++ react_native/flat-list/components/CartList.jsx
@@ -1,16 +1,10 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { dummy_data } from '../data/dummy'
 import CartItem from './CartItem';
 
 // CartList: 장바구니 목록 전체를 보여주는 컴포넌트
 // FlatList를 사용해 데이터 배열을 효율적으로 렌더링함
 // ScrollView와 달리 FlatList는 화면에 보이는 항목만 렌더링하므로 성능이 좋음
-const CartList = () => {
-
-  // useState로 장바구니 목록 상태 관리
-  // cartList: 현재 장바구니 데이터, setCartList: 데이터 변경 함수
-  const[cartList, setCartList] = useState(dummy_data);
+const CartList = ({cartList, updateCart}) => {
 
   return (
     <View style={styles.listContainer}>
@@ -21,11 +15,11 @@ const CartList = () => {
           - contentContainerStyle: 반복 렌더링되는 항목들을 감싸는 영역 스타일 */}
       <FlatList
         data={cartList}
-        renderItem={({item}) => <CartItem item={item}/>}
-        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <CartItem item={item} updateCart={updateCart}/>}
+        keyExtractor={(item) => item.id.toString()}
         style={styles.list}
 
-        {/* 반복적으로 그려지는 컴포넌트를 감싸는 전체 영역 디자인 */}
+        // {/* 반복적으로 그려지는 컴포넌트를 감싸는 전체 영역 디자인 */}
         contentContainerStyle={{
           gap : 12
         }}
@@ -39,7 +33,7 @@ export default CartList
 const styles = StyleSheet.create({
   // 목록 전체 컨테이너 스타일
   listContainer : {
-    width : '90%',
+    width : '100%',
     marginHorizontal : 'auto', // 수평 중앙 정렬
     padding : 12,
     backgroundColor : '#dddddd'
